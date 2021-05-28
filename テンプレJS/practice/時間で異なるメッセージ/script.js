f8214b9e53722796b836f9933da9900fd85e42c6
trust <44katsu26@gmail.com>
2021-05-28 17:31:31 +0900

    更新ver.1.1

--- テンプレJS/practice/時間で異なるメッセージ/script.js
+++ テンプレJS/practice/時間で異なるメッセージ/script.js
@@ -1,1 +1,9 @@
-var
+var hour = new Date().getHours();
+
+if (hour >= 16 && hour <= 19) {
+    window.alert("もうすぐ退社の時間だよ！！");
+} else if (hour === 12 || hour === 15) {
+    window.alert("そろそろ休憩しよう！！")
+} else {
+    window.alert("お仕事頑張ってください！ファイト！！")
+}
