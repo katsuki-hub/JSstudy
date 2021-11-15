7d1baa5628d43fc7805d83aa65ce504fb7ca604d
trust <44katsu26@gmail.com>
2021-11-15 13:17:56 +0900

    配列要素に関数ADD

---
+++ php/arrayfunction.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-P2ZWXCZ');
+  </script>
+  <!-- End Google Tag Manager -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="IE=edge">
+  <title>PHP編”配列の要素に関数”</title>
+  <meta name=”description” content=”PHP編の学習技術ブログです。”>
+  <meta name="keywords" content="PHP,プログラミング,技術ブログ,PHP超入門編,ソースコード" />
+  <link href="../css/style.css" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="android-touch-icon.png">
+  <meta property="og:title" content="プログラミング学習帳～簡単解説！JavaScript初級コード～">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="JavaScript超入門編の学習技術ブログです。どんなソースコードで作成されているのか？ソースコードと概要を分かりやすく説明しています">
+  <meta property="og:url" content="https://katsu-study.work/">
+  <meta property="og:site_name" content="JavaScript学習帳">
+  <meta property="og:image" content="https://katsu-study.work/images/js.png">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="JSプログラミング講座">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="manifest" href="../manifest.json">
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2ZWXCZ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <header>
+    <div class="header-contents">
+      <h1>配列の各要素に関数適用</h1>
+      <h2>PHPのシンタックス</h2>
+    </div><!-- /.header-contents -->
+    <div class="btn" id="open_btn">
+      <label class="menu-btn"><span></span></label>
+    </div>
+
+    <div id="boxmenu">
+      <nav class="phpnav">
+        <ul class="menu_1">
+          <li><a href="syntax.php">制御構造</a></li>
+          <li><a href="function.php">関数</a></li>
+          <li><a href="string.php">文字列</a></li>
+          <li><a href="convert.php">文字列の変換</a></li>
+          <li><a href="comparison.php">文字列の比較</a></li>
+          <li><a href="search.php">文字列の検索</a></li>
+          <li><a href="regex.php">正規表現</a></li>
+          <li><a href="array.php">配列</a></li>
+          <li><a href="array02.php">配列の要素</a></li>
+          <li><a href="arrayextract.php">配列の抽出</a></li>
+          <li><a href="arraysort.php">配列をソート</a></li>
+          <li><a href="arraysearch.php">配列の比較・検索</a></li>
+          <li><a href="arrayfunction.php">配列の要素に関数適用</a></li>
+        </ul>
+
+        <div class="copyright">
+          <small>&copy; 2021 かつまる学習帳</small>
+        </div>
+      </nav>
+    </div><!-- /boxmenu -->
+  </header>
+
+  <!-- パンくずリスト -->
+  <div id="bread">
+    <ol>
+      <li><a href="../index.html">HOME</a></li>
+      <li><a href="arrayfunction.php">配列の要素に関数</a></li>
+    </ol>
+  </div>
+  <div class="main-wrapper">
+    <article>
+      <section>
+        <h2>配列の要素で関数を繰り返し実行する</h2>
+        <div class="frame3">
+          array_walk()は各要素を引数にして指定の関数を繰り返し実行します。これはforeach文の配列から値を取り出して繰り返すのと似ています。
+          <div class="frame2">
+            <b>各要素を引数にして関数を繰り返し実行する</b><br>
+            $result = array_walk(&$array,$callBack,$userdata);
+          </div>
+          $arrayの配列から1つ要素を取り出し、それを引数として$callBackで指定した関数を実行します。実行後、次の要素を取り出し、その要素を引数として再び$callBack関数で実行します。この操作は配列の要素の数だけ繰り返します。※引数で渡された配列$arrayの値を直接書き換える<br>$userdataはオプションですが、$callBack関数の第3引数として渡すことができる値です。<br>戻り値の$resultはarray_walk()の処理が成功した時true、失敗した時falseが返る
+          <div class="frame2">
+            <b>コールバック関数</b><br>
+            function 関数名($value,$key,$userdata) {<br>
+            　処理文<br>
+            }<br>
+            array_walk(&[キー1 => 値1,...],myFunc,引数);<br>
+            　function myFunc($value,$key,$userdata) {<br>
+            　処理文<br>
+            }
+          </div>
+        </div>
+        <h3>配列の値をドル換算してリスト表示する</h3>
+        <?php
+        function exchabgeList($value, $key, $rateData) {
+          $rate = $rateData["rate"];
+          if ($rate == 0) {
+            return;
+          }
+          $price = $value / $rate;
+          $exPrice = sprintf('%.02f', $price); //下2桁まで表示する書式
+          echo "<li>", $rateData["symbol"], $exPrice, "</li>"; //<li>タグと通貨シンボルをつける
+        }
+
+        $priceList = [5000, 30000, 240000];
+        $dollaryen = ["symbol" => "$", "rate" => 113.86];
+        echo "<ul>";
+        array_walk($priceList, "exchabgeList", $dollaryen);
+        echo "</ul>";
+        ?>
+        <!-- ソースコード -->
+        <pre><code class="prettyprint">&lt;?php
+function exchabgeList($value, $key, $rateData) {
+  $rate = $rateData[&quot;rate&quot;];
+  if ($rate == 0) {
+    return;
+  }
+  $price = $value / $rate;
+  $exPrice = sprintf(&#039;%.02f&#039;, $price); //下2桁まで表示する書式
+  echo &quot;&lt;li&gt;&quot;, $rateData[&quot;symbol&quot;], $exPrice, &quot;&lt;/li&gt;&quot;; //&lt;li&gt;タグと通貨シンボルをつける
+}
+
+$priceList = [5000, 30000, 240000];
+$dollaryen = [&quot;symbol&quot; =&gt; &quot;$&quot;, &quot;rate&quot; =&gt; 113.86];
+echo &quot;&lt;ul&gt;&quot;;
+array_walk($priceList, &quot;exchabgeList&quot;, $dollaryen);
+echo &quot;&lt;/ul&gt;&quot;;
+?&gt;
+</code></pre>
+        <div class="blank"></div>
+
+        <h2>配列の要素全てに同じ関数を適用する</h2>
+        <div class="frame3">
+        <div class="frame2">
+          <b>配列の個々の値でコールバック関数を実行する</b><br>
+          $result = array_map($callBack,$array);
+        </div>
+        array_map()には2つの使い方があります。1つは指定した配列の要素にコールバック関数を適用したいときです。<br>※引数で与えた配列を直接書き換えるのではなく、コールバック関数で処理した配列が$resultに入ります。
+        <div class="frame2">
+          <b>コールバック関数</b><br>
+          function 関数名($value) {<br>
+            　処理文<br>
+            　return 値<br>
+          }
+        </div>
+        </div>
+        <?php
+        ?>
+
+
+
+
+      </section>
+    </article>
+  </div><!-- /.main-wrapper -->
+  <footer><small>&copy;かつまる学習帳</small></footer>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+  <script src="../scripts/move.js"></script>
+</body>
+
+</html>
--- php/arraysearch.php
+++ php/arraysearch.php
@@ -233,9 +233,180 @@ print_r($myList);
         </div>
 
         <h3>見つかったキーで別の配列から値を取り出す</h3>
+        <?php
+        $nameList = ["m01" => "桃太郎", "m02" => "浦島太郎", "w01" => "織姫", "w02" => "月姫"];
+        $ageList = ["m01" => 16, "m02" => 55, "w01" => 20, "w02" => 25];
 
+        function getAge($name)
+        {
+          global $nameList;
+          global $ageList;
+          $key = array_search($name, $nameList);
+          if ($key !== false) { // !== 値または型が等しくない
+            $age = $ageList[$key];
+            echo "{$name}さんは{$age}歳です。";
+          } else {
+            echo "「{$name}」はメンバーではない。";
+          }
+        }
+        echo getAge("桃太郎"), "\n <br>";
+        echo getAge("浦島太郎"), "\n <br>";
+        echo getAge("織姫"), "\n <br>";
+        echo getAge("赤鬼"), "\n <br>";
+        ?>
+        <!-- ソースコード -->
+        <pre><code class="prettyprint">&lt;?php
+$nameList = [&quot;m01&quot; =&gt; &quot;桃太郎&quot;, &quot;m02&quot; =&gt; &quot;浦島太郎&quot;, &quot;w01&quot; =&gt; &quot;織姫&quot;, &quot;w02&quot; =&gt; &quot;月姫&quot;];
+$ageList = [&quot;m01&quot; =&gt; 16, &quot;m02&quot; =&gt; 55, &quot;w01&quot; =&gt; 20, &quot;w02&quot; =&gt; 25];
 
+function getAge($name)
+{
+  global $nameList;
+  global $ageList;
+  $key = array_search($name, $nameList);
+  if ($key !== false) { // !== 値または型が等しくない
+    $age = $ageList[$key];
+    echo &quot;{$name}さんは{$age}歳です。&quot;;
+  } else {
+    echo &quot;「{$name}」はメンバーではない。&quot;;
+  }
+}
+echo getAge(&quot;桃太郎&quot;),&quot;\n &lt;br&gt;&quot;;
+echo getAge(&quot;浦島太郎&quot;),&quot;\n &lt;br&gt;&quot;;
+echo getAge(&quot;織姫&quot;),&quot;\n &lt;br&gt;&quot;;
+echo getAge(&quot;赤鬼&quot;),&quot;\n &lt;br&gt;&quot;;
+?&gt;
+</code></pre>
+        <div class="blank"></div>
 
+        <h2>配列を比較して一致しない値を見つける</h2>
+        <div class="frame3">
+          array_diff()を使うことで、配列Aと配列Bを比較して配列Aの中から配列Bにはない値を見つけ出すことができます。
+        </div>
+        <h3>aとｂのどちらにもない値を取り出す</h3>
+        <?php
+        $checkID = ["a11", "b22", "c33", "d44"];
+        $aList = ["b22", "c15", "a12", "d55"];
+        $bList = ["a8", "c22", "c55", "d44"];
+
+        $diffID = array_diff($checkID, $aList, $bList);
+        foreach ($diffID as $value) {
+          echo "{$value}は新規です。\n <br>";
+        }
+        ?>
+        <!-- ソースコード -->
+        <pre><code class="prettyprint">&lt;?php
+$checkID = [&quot;a11&quot;, &quot;b22&quot;, &quot;c33&quot;, &quot;d44&quot;];
+$aList = [&quot;b22&quot;, &quot;c15&quot;, &quot;a12&quot;, &quot;d55&quot;];
+$bList = [&quot;a8&quot;, &quot;c22&quot;, &quot;c55&quot;, &quot;d44&quot;];
+
+$diffID = array_diff($checkID, $aList, $bList);
+foreach ($diffID as $value) {
+  echo &quot;{$value}は新規です。\n &lt;br&gt;&quot;;
+}
+?&gt;
+</code></pre>
+        <div class="blank"></div>
+
+        <h2>配列の値を検索置換する</h2>
+        <div class="frame3">
+          <a href="search.php">「文字列の検索」</a>でstr_replace(),str_ireplace()を使って文字列を置換したが、配列の検索置換も行えます。検索できるのはインデックス配列の値のみで、連想配列の値を検索したい場合はpreg_replace()を使う。
+        </div>
+        <h3>配列の値を検索置換して表示する</h3>
+        <?php
+        $data = ["ミネラルウォーター", "SSD250G", "HDD500G"];
+        $seach = ["ミネラル", "SSD", "HDD"];
+        $replacement = ["飲み水", "ソリッド・ステート・ドライブ", "ハード・ディスク・ドライブ"];
+        $result = str_replace($seach, $replacement, $data);
+        echo "社内備品：\n <br>";
+        echo $result[0], "、", $result[1], "、", $result[2];
+        ?>
+        <!-- ソースコード -->
+        <pre><code class="prettyprint">&lt;?php
+$data = [&quot;ミネラルウォーター&quot;, &quot;SSD250G&quot;, &quot;HDD500G&quot;];
+$seach = [&quot;ミネラル&quot;, &quot;SSD&quot;, &quot;HDD&quot;];
+$replacement = [&quot;飲み水&quot;, &quot;ソリッド・ステート・ドライブ&quot;, &quot;ハード・ディスク・ドライブ&quot;];
+$result = str_replace($seach, $replacement, $data);
+echo &quot;社内備品：\n &lt;br&gt;&quot;;
+echo $result[0], &quot;、&quot;, $result[1], &quot;、&quot;, $result[2];
+?&gt;
+</code></pre>
+        <div class="blank"></div>
+
+        <h2>正規表現を使って配列を検索する(部分一致検索)</h2>
+        <div class="frame3">
+          preg_grep()は正規表現を使って配列を検索しましたが、配列の検索も可能です。文字列の部分一致、大文字小文字を区別せずに検索、マッチした複数の値を取り出すといったことが可能になる。<br>preg_grep()の第3引数にPREG_GREP_INVERTを指定すると、マッチしなかった値を配列として取り出すことができます。
+        </div>
+        <h3>配列から「太郎」の付く名前を取り出す</h3>
+        <?php
+        $nameList = ["桃太郎", "一寸法師", "浦島太郎", "赤鬼", "金太郎"];
+        $pattern = "/太郎/";
+        $result = preg_grep($pattern, $nameList);
+        $noresult = preg_grep($pattern, $nameList, PREG_GREP_INVERT);
+        echo "<b>該当者</b>" . count($result) . "件\n <br>";
+        foreach ($result as $value) {
+          echo $value, "\n <br>";
+        }
+        echo "<b>該当しない</b>" . count($noresult) . "件\n <br>";
+        foreach ($noresult as $value) {
+          echo $value, "\n <br>";
+        }
+        ?>
+        <!-- ソースコード -->
+        <pre><code class="prettyprint">&lt;?php
+$nameList = [&quot;桃太郎&quot;, &quot;一寸法師&quot;, &quot;浦島太郎&quot;, &quot;赤鬼&quot;, &quot;金太郎&quot;];
+$pattern = &quot;/太郎/&quot;;
+$result = preg_grep($pattern, $nameList);
+$noresult = preg_grep($pattern, $nameList, PREG_GREP_INVERT);
+echo &quot;&lt;b&gt;該当者&lt;/b&gt;&quot; . count($result) . &quot;件\n &lt;br&gt;&quot;;
+foreach ($result as $value) {
+  echo $value, &quot;\n &lt;br&gt;&quot;;
+}
+echo &quot;&lt;b&gt;該当しない&lt;/b&gt;&quot; . count($noresult) . &quot;件\n &lt;br&gt;&quot;;
+foreach ($noresult as $value) {
+  echo $value, &quot;\n &lt;br&gt;&quot;;
+}
+?&gt;
+</code></pre>
+        <div class="blank"></div>
+
+        <h2>配列の値を正規表現で検索置換する</h2>
+        <div class="frame3">
+          preg_replace()も配列の値を正規表現で置換可能。<br>※配列の値は書き換えではなく新しい配列を返す！！<br>先程のstr_replace()と違って、連想配列の値を検索できます。
+        </div>
+        <h3>電話番号の末尾4桁を伏字にして表示する</h3>
+        <?php
+        $data = [];
+        $data[] = ["name" => "桃太郎", "age" => 18, "phone" => "080-8888-8888"];
+        $data[] = ["name" => "一寸法師", "age" => 32, "phone" => "090-1919-1919"];
+        $data[] = ["name" => "浦島太郎", "age" => 40, "phone" => "092-333-4444"];
+        $data[] = ["name" => "赤鬼", "age" => 55, "phone" => "0120-888-8888"];
+        $pattern = "/(-)\d{4}$/";
+        $replacement = "$1****";
+        foreach ($data as $user) {
+          $result = preg_replace($pattern, $replacement, $user); //番号末尾4桁伏せ字
+          foreach ($result as $key => $value) {
+            echo "{$key}：", $value, "\n <br>";
+          }
+        }
+        ?>
+        <!-- ソースコード -->
+<pre><code class="prettyprint">&lt;?php
+$data = [];
+$data[] = [&quot;name&quot; =&gt; &quot;桃太郎&quot;, &quot;age&quot; =&gt; 18, &quot;phone&quot; =&gt; &quot;080-8888-8888&quot;];
+$data[] = [&quot;name&quot; =&gt; &quot;一寸法師&quot;, &quot;age&quot; =&gt; 32, &quot;phone&quot; =&gt; &quot;090-1919-1919&quot;];
+$data[] = [&quot;name&quot; =&gt; &quot;浦島太郎&quot;, &quot;age&quot; =&gt; 40, &quot;phone&quot; =&gt; &quot;092-333-4444&quot;];
+$data[] = [&quot;name&quot; =&gt; &quot;赤鬼&quot;, &quot;age&quot; =&gt; 55, &quot;phone&quot; =&gt; &quot;0120-888-8888&quot;];
+$pattern = &quot;/(-)\d{4}$/&quot;;
+$replacement = &quot;$1****&quot;;
+foreach ($data as $user) {
+  $result = preg_replace($pattern, $replacement, $user); //番号末尾4桁伏せ字
+  foreach ($result as $key =&gt; $value) {
+    echo &quot;{$key}：&quot;, $value, &quot;\n &lt;br&gt;&quot;;
+  }
+}
+?&gt;
+</code></pre><br>
       </section>
     </article>
   </div><!-- /.main-wrapper -->
